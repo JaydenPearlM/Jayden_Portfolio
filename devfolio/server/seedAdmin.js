c79ed60a77de5b8a41c19d1b3fae3bfc9fe1f236
https://github.com/JaydenPearlM <maxwellpearl6790@gmail.com>
2025-10-10 23:05:25 -0400

    Last commit for the night

--- devfolio/server/seedAdmin.js
+++ devfolio/server/seedAdmin.js
@@ -5,8 +5,8 @@ require('dotenv').config();
 
 async function seed() {
   await mongoose.connect(process.env.MONGODB_URI);
-  const hash = await bcrypt.hash('YourStrongPassword!', 12);
-  await new Admin({ username: 'admin', passwordHash: hash }).save();
+  const hash = await bcrypt.hash('resU_nimda', 12);
+  await new Admin({ username: 'admin_User', passwordHash: hash }).save();
   console.log('✅ Admin user created');
   mongoose.disconnect();
 }
